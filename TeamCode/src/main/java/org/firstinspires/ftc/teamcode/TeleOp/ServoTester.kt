b672c428e388a7b3801ec847da2a5a3140a65fba
Markytt <theinteruptingchicken@gmail.com>
2024-09-28 11:02:09 -0700

    small change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ServoTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ServoTester.kt
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo
 @TeleOp(name = "ServoPositionTester", group = "Basic Chassis")
 class ServoTester : LinearOpMode() {
     override fun runOpMode() {
-        val airplaneServo = hardwareMap.get(Servo::class.java, "airplaneServo")
+        val airplaneServo = hardwareMap.get(Servo::class.java, "launchservo")
 
         telemetry.addData("Status", "Initialized")
         telemetry.update()
