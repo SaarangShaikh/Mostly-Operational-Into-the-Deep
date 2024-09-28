d8a7326ab96deeb36338955c084ff304647b1b80
18119 Mostly Operational <64237204+MostlyOperational18119@users.noreply.github.com>
2024-09-27 17:53:03 -0700

    wtf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTester.kt
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 @TeleOp(name = "MotorTester", group = "0000000")
 class MotorTester : LinearOpMode() {
@@ -17,6 +19,7 @@ class MotorTester : LinearOpMode() {
         val BR = hardwareMap.get(DcMotor::class.java, "motorBR")
         BR.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         BR.mode = DcMotor.RunMode.RUN_USING_ENCODER
+        BR.direction = DcMotorSimple.Direction.REVERSE
         BL.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         BL.mode = DcMotor.RunMode.RUN_USING_ENCODER
         FR.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
@@ -26,19 +29,40 @@ class MotorTester : LinearOpMode() {
 
         waitForStart()
         while(opModeIsActive()) {
+            var leftX = gamepad1.left_stick_x.toDouble()
+            var leftY = -gamepad1.left_stick_y.toDouble()
+            var rightX = gamepad1.right_stick_x.toDouble()
+
             if (gamepad1.a){
                 BR.power = 1.0
+                telemetry.addLine("BR")
             }
             if (gamepad1.b){
                 BL.power = 1.0
+                telemetry.addLine("BL")
             }
             if (gamepad1.x){
                 FR.power = 1.0
+                telemetry.addLine("FR")
             }
             if (gamepad1.y){
                 FL.power = 1.0
+                telemetry.addLine("FL")
             }
 
+            FL?.power = (leftY + leftX + rightX)
+            BL?.power = (leftY - leftX + rightX)
+            FR?.power = (leftY - leftX - rightX)
+            BR?.power = (leftY + leftX - rightX)
+
+
+            telemetry.addData("BL Power: ",  BL.power)
+            telemetry.addData("BR Power:  ",  BR.power)
+            telemetry.addData("FL Power: ",  FL.power)
+            telemetry.addData("FR Power:  ",  FR.power)
+            telemetry.addData("Left Stick Y:  ",  gamepad1.left_stick_y)
+            telemetry.addData("Left Stick X:  ",  gamepad1.left_stick_x)
+            telemetry.addData("Right Stick X:  ",  gamepad1.right_stick_x)
             telemetry.addLine("OpMode is active")
             telemetry.update()
         }
